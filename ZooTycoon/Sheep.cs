a5812f891fb284c3cfa8036eb6061df3da9436d4
blocwer <blocwer123@yandex.ru>
2022-12-16 17:57:44 +0700

    ZT-1: Добавление овцы

---
+++ ZooTycoon/Sheep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooTycoon
+{
+    internal class Sheep : Animal
+    {
+
+        public Sheep(string name, Gender gender) : base("Овца", "Овечка", name, 2, 140M, gender)
+        {
+
+        }
+
+        public override void Feed(Food foodObj)
+        {
+            if (foodObj.TypeFood == FoodList.Seed)
+                Eat(foodObj);
+            else
+                ThrowUp(foodObj);
+        }
+    }
+}
